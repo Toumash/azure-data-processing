5e1ef1da6711aeced03ef3f23f128455e34e5a52
Tomasz Dłuski <mr.toumash@gmail.com>
2020-06-25 18:58:09 +0200

    initial

---
+++ WSB.DataProcessingIngest/Function1.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace WSB.DataProcessingIngest
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log, [CosmosDB("Fagkveld", "ViktigeData", CreateIfNotExists = true, ConnectionStringSetting = "Default")] out dynamic document)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            document = new { xd = DateTime.Now };
+        }
+    }
+}
